c6b1fc88553267cf732f5bf98cf149bd825fda10
김혜준 <alice5855@naver.com>
2023-02-15 20:44:57 +0900

    FixName2

--- src/components/Frontcard.jsx
+++ src/components/Frontcard.jsx
@@ -1,22 +1,18 @@
 import * as S from './Frontcard.styled';
-//import HeartButton from './HeartButton';
 
 function Frontcard({ pokemonDatas }) {
-	//console.log('프론트' + pokemonDatas);
 	return (
 		<div>
 			<S.FrontCardForm>
 				<S.PokemonId># {pokemonDatas.id}</S.PokemonId>
-				{/* <button type='checkbox'>담기</button> */}
 				<S.CardTitle>{pokemonDatas.name}</S.CardTitle>
-
 				<S.CardList>
 					<div>Height : {pokemonDatas.height / 10}m</div>
 					<div>Weight : {pokemonDatas.weight / 10}kg</div>
 				</S.CardList>
 				<S.PokemonPicture
 					src={pokemonDatas.sprites.front_default}
-					alt='포켓몬 정면 사진'
+					alt='Waiting for future updates...'
 				></S.PokemonPicture>
 				<S.PokemonTypes>
 					{pokemonDatas.types.map((pokemonTypes, index) => (
